a63ef91ec764be7bc95a6c18206f54e60cb2a824
Damon German <damon.german@hyland.com>
2023-09-23 16:56:10 -0500

    Add AccountsTests project with initial test
    A new AccountsTests project was added in the solution file. The project includes several classes (Account, SavingsAccount, and CheckingAccount) which contain the initial configuration for respective accounts. An initial unit test was added to check SavingsAccount's InterestRate using NUnit's Assert. The test project also includes references to necessary test packages in the AccountsTests.csproj file. Future developments should extend this initial setup to expand the testing coverage and to validate different aspects of account classes.

---
+++ AccountsTests/AccountsTests.cs
@@ -0,0 +1,33 @@
+namespace AccountsTests;
+
+public class Account
+{
+    // Common properties for all accounts
+}
+
+public class SavingsAccount : Account
+{
+    public double InterestRate { get; init; } = 0.05; // 5% interest rate
+}
+
+public class CheckingAccount : Account
+{
+    public double InterestRate { get; init; } = 0.02; // 2% interest rate
+}
+
+[TestFixture]
+public class AccountTests
+{
+    [Test]
+    public void IsOperatorPatternMatching()
+    {
+        Account account = new SavingsAccount(); // This could be changed to CheckingAccount for testing
+
+        if (account is SavingsAccount savingsAccount)
+        {
+            Assert.That(savingsAccount.InterestRate, Is.EqualTo(0.05));
+        }
+        
+        // TODO: Create a test for CheckingAccount using the same pattern matching technique
+    }
+}
